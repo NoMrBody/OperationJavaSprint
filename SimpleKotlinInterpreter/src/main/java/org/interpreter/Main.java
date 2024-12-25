4f60e1a5ce496cb157b0fb6e927d0b73f684375d
th3g3ntl3man <kutchukhidze.nik@gmail.com>
2024-12-25 21:04:31 +0400

    add Lexer class

--- SimpleKotlinInterpreter/src/main/java/org/interpreter/Main.java
+++ SimpleKotlinInterpreter/src/main/java/org/interpreter/Main.java
@@ -1,7 +1,12 @@
 package org.interpreter;
 
+import org.interpreter.lexer.Lexer;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Lexer lexer = new Lexer("sum = 5 + 10");
+        Lexer lexer2 = new Lexer("if (x >= 5) println(x)");
+        System.out.println(lexer.tokenize());
+        System.out.println(lexer2.tokenize());
     }
 }
