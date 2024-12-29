4fa6a144a32ef823302e6787c9cb19631b93575f
th3g3ntl3man <kutchukhidze.nik@gmail.com>
2024-12-30 01:24:21 +0400

    sample input

--- SimpleKotlinInterpreter/src/main/java/org/interpreter/Main.java
+++ SimpleKotlinInterpreter/src/main/java/org/interpreter/Main.java
@@ -1,6 +1,41 @@
 package org.interpreter;
 
+import org.interpreter.lexer.*;
+import org.interpreter.parser.*;
+import org.interpreter.evaluator.*;
+import org.interpreter.syntaxtree.*;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        String code =
+                """
+    var num = 1237
+    var reversed = 0
+
+    while (num != 0) {
+        val digit = num % 10
+        reversed = reversed * 10 + digit
+        num = num / 10
     }
+    println(reversed)
+            """;
+
+        Lexer lexer = new Lexer(code);
+        List<Token> tokens = lexer.tokenize();
+//        System.out.println("Tokens: " + tokens);
+
+        Parser parser = new Parser(tokens);
+        try {
+            Node syntaxTree = parser.parse();
+//            System.out.println("Parsed syntax tree: " + syntaxTree);
+
+            Evaluator evaluator = new Evaluator();
+            evaluator.eval(syntaxTree);
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
 }
